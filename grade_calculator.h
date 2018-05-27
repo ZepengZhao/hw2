d5bdf30b2d8cc42b0571fefc610cdb3563ace3f9
ZepengZhao <email@example.com>
2018-05-26 21:43:00 -0700

    almost done

---
+++ grade_calculator.h
@@ -0,0 +1,40 @@
+#include <QApplication>
+#include <QLabel>
+#include <QPushButton>
+#include <QLineEdit>
+#include <QHBoxLayout>
+#include <QSpinBox>
+#include <QSlider>
+#ifndef GRADE_CALCULATOR_H
+#define GRADE_CALCULATOR_H
+
+#include <QMainWindow>
+
+namespace Ui {
+    class grade_calculator;
+}
+
+class grade_calculator : public QMainWindow
+{
+    Q_OBJECT
+
+   public:
+        explicit grade_calculator(QWidget *parent = 0);
+      //  ~grade_calculator();
+
+public slots:
+        void update_overall();
+
+private slots:
+    void on_schemaA_clicked();
+
+    void on_clear_clicked();
+
+    void on_schemaB_clicked();
+
+private:
+     Ui::grade_calculator *ui;
+     bool schemaA = true;
+};
+
+#endif // GRADE_CALCULATOR_H
---
+++ main.cpp
@@ -0,0 +1,18 @@
+#include <QApplication>
+#include <QMainWindow>
+#include <QLabel>
+#include <QPushButton>
+#include <QLineEdit>
+#include <QHBoxLayout>
+#include <QSpinBox>
+#include <QSlider>
+#include "grade_calculator.h"
+int main(int argc, char *argv[])
+{
+    QApplication app(argc, argv);
+    grade_calculator w1;
+
+    w1.show();
+
+    return app.exec();   // <-- This part handles `signals` and `events`.
+}
